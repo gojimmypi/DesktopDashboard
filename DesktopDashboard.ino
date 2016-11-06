69e5802d3619d1970005fcb59b82b76a4612c9fd
gojimmypi <gojimmypi@gmail.com>
2016-11-06 10:05:05 -0800

    initial prototype with Raspberry Pi ILI9341 Display Module

--- DesktopDashboard.ino
+++ DesktopDashboard.ino
@@ -3,6 +3,7 @@
 // View - Team Explorer; Publish to GitHub (Get Started...)
 
 // copy / paste Adafruit graphictest.ino  (see Arduino File-Examples...)
+// if Arduino environment properly installed, simply vMicro-Build to automaticlly resolve references (no need to manually vMicro-Add Library...)
 
 /***************************************************
 This is our GFX example for the Adafruit ILI9341 Breakout and Shield
@@ -20,18 +21,71 @@ MIT license, all text above must be included in any redistribution
 ****************************************************/
 
 
+//#include "font_Arial.h"
+//#include "ImageViewer.h"
 #include "SPI.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9341.h"
+#include "font_Arial.h"
+#include "FreeSansBold24pt7b.h"
 
-// For the Adafruit shield, these are the default.
-#define TFT_DC 9
-#define TFT_CS 10
+//// For the Adafruit shield, these are the default.
+//#define TFT_DC 9
+//#define TFT_CS 10
+// For the esp shield, these are the default.
+#define TFT_DC 2
+#define TFT_CS 15
+
+#define Touch_CS 4
+#define Touch_IRQ 5
+#define SD_CS 9
 
 // Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 // If using the breakout, change pins as desired
 //Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
+
+// https://www.adafruit.com/product/1601
+// https://www.adafruit.com/product/2298
+
+// CONNECTOR VIEW FROM BACK
+//ESP8266     Name   Display Pins  Name     ESP8266
+//----------  -----   -----------  -------  -----------
+//D2 - GPIO4  TP CS    26     25   GND
+//D8 - GPIO15 LCD CS   24     23   SPI CLK  D5 - GPIO14
+//D4 - GPIO2  DC RS    22     21   MISO     D6 - GPIO12
+//                     20     19   MOSI     D7 - GPIO13
+//D1 - GPIO5           18     17
+//                     16     15            not used
+//                     14     13   reset
+//                     12     11            not used
+//               Rx    10      9   GND
+//               Tx     8      7  
+//                      6      5
+//               +5     4      3
+//+5 Vin/Vcc     +5     2      1   3.3V     3v3 
+
+// SOLDER PIN VIEW FROM DISPLAY SIDE 
+//ESP8266		     Display Pins		     ESP8266
+//            GND      25     26   TP CS    D2 - GPIO4
+//D5 - GPIO14 SPI CLK  23     24   LCD CS   D8 - GPIO15
+//D6 - GPIO12 MISO     21     22   DCRS     D4 - GPIO2
+//D7 - GPIO13 MOSI     19     20
+//                     17     18   TP IRQ	D1 - GPIO5
+//      not used       15     16
+//         reset       13     14
+//      not used       11     12
+//           GND        9     10   Rx
+//                      7      8   Tx
+//                      5      6              GND
+//                      3      4   +5         
+//           + 3        1	   2   +5         +5 Vin
+
+
+
+
+
+
 
 void setup() {
 	Serial.begin(9600);
@@ -41,64 +95,64 @@ void setup() {
 
 	// read diagnostics (optional but can help debug problems)
 	uint8_t x = tft.readcommand8(ILI9341_RDMODE);
-	Serial.print("Display Power Mode: 0x"); Serial.println(x, HEX);
+	Serial.print("Display Power Mode: 0x"); Serial.println(x, HEX); // success =  0x9C
 	x = tft.readcommand8(ILI9341_RDMADCTL);
-	Serial.print("MADCTL Mode: 0x"); Serial.println(x, HEX);
+	Serial.print("MADCTL Mode: 0x"); Serial.println(x, HEX); // Sucess =  0x48
 	x = tft.readcommand8(ILI9341_RDPIXFMT);
-	Serial.print("Pixel Format: 0x"); Serial.println(x, HEX);
+	Serial.print("Pixel Format: 0x"); Serial.println(x, HEX); // Success =  0x5
 	x = tft.readcommand8(ILI9341_RDIMGFMT);
-	Serial.print("Image Format: 0x"); Serial.println(x, HEX);
+	Serial.print("Image Format: 0x"); Serial.println(x, HEX); // Success = 0x0
 	x = tft.readcommand8(ILI9341_RDSELFDIAG);
-	Serial.print("Self Diagnostic: 0x"); Serial.println(x, HEX);
+	Serial.print("Self Diagnostic: 0x"); Serial.println(x, HEX); // Success =  0x0
 
-	Serial.println(F("Benchmark                Time (microseconds)"));
-	delay(10);
-	Serial.print(F("Screen fill              "));
-	Serial.println(testFillScreen());
-	delay(500);
+																 //Serial.println(F("Benchmark                Time (microseconds)"));
+																 //delay(10);
+																 //Serial.print(F("Screen fill              "));
+																 //Serial.println(testFillScreen());
+																 //delay(500);
 
-	Serial.print(F("Text                     "));
-	Serial.println(testText());
-	delay(3000);
+																 //Serial.print(F("Text                     "));
+																 //Serial.println(testText());
+																 //delay(3000);
 
-	Serial.print(F("Lines                    "));
-	Serial.println(testLines(ILI9341_CYAN));
-	delay(500);
+																 //Serial.print(F("Lines                    "));
+																 //Serial.println(testLines(ILI9341_CYAN));
+																 //delay(500);
 
-	Serial.print(F("Horiz/Vert Lines         "));
-	Serial.println(testFastLines(ILI9341_RED, ILI9341_BLUE));
-	delay(500);
+																 //Serial.print(F("Horiz/Vert Lines         "));
+																 //Serial.println(testFastLines(ILI9341_RED, ILI9341_BLUE));
+																 //delay(500);
 
-	Serial.print(F("Rectangles (outline)     "));
-	Serial.println(testRects(ILI9341_GREEN));
-	delay(500);
+																 //Serial.print(F("Rectangles (outline)     "));
+																 //Serial.println(testRects(ILI9341_GREEN));
+																 //delay(500);
 
-	Serial.print(F("Rectangles (filled)      "));
-	Serial.println(testFilledRects(ILI9341_YELLOW, ILI9341_MAGENTA));
-	delay(500);
+																 //Serial.print(F("Rectangles (filled)      "));
+																 //Serial.println(testFilledRects(ILI9341_YELLOW, ILI9341_MAGENTA));
+																 //delay(500);
 
-	Serial.print(F("Circles (filled)         "));
-	Serial.println(testFilledCircles(10, ILI9341_MAGENTA));
+																 //Serial.print(F("Circles (filled)         "));
+																 //Serial.println(testFilledCircles(10, ILI9341_MAGENTA));
 
-	Serial.print(F("Circles (outline)        "));
-	Serial.println(testCircles(10, ILI9341_WHITE));
-	delay(500);
+																 //Serial.print(F("Circles (outline)        "));
+																 //Serial.println(testCircles(10, ILI9341_WHITE));
+																 //delay(500);
 
-	Serial.print(F("Triangles (outline)      "));
-	Serial.println(testTriangles());
-	delay(500);
+																 //Serial.print(F("Triangles (outline)      "));
+																 //Serial.println(testTriangles());
+																 //delay(500);
 
-	Serial.print(F("Triangles (filled)       "));
-	Serial.println(testFilledTriangles());
-	delay(500);
+																 //Serial.print(F("Triangles (filled)       "));
+																 //Serial.println(testFilledTriangles());
+																 //delay(500);
 
-	Serial.print(F("Rounded rects (outline)  "));
-	Serial.println(testRoundRects());
-	delay(500);
+																 //Serial.print(F("Rounded rects (outline)  "));
+																 //Serial.println(testRoundRects());
+																 //delay(500);
 
-	Serial.print(F("Rounded rects (filled)   "));
-	Serial.println(testFilledRoundRects());
-	delay(500);
+																 //Serial.print(F("Rounded rects (filled)   "));
+																 //Serial.println(testFilledRoundRects());
+																 //delay(500);
 
 	Serial.println(F("Done!"));
 
@@ -106,11 +160,17 @@ void setup() {
 
 
 void loop(void) {
-	for (uint8_t rotation = 0; rotation<4; rotation++) {
-		tft.setRotation(rotation);
-		testText();
-		delay(1000);
-	}
+	tft.setRotation(1);
+
+	testText();
+	delay(2000);
+}
+
+
+void screenDashboard() {
+	tft.fillScreen(ILI9341_BLACK);
+	tft.setCursor(0, 0);
+
 }
 
 unsigned long testFillScreen() {
@@ -131,27 +191,18 @@ unsigned long testFillScreen() {
 unsigned long testText() {
 	tft.fillScreen(ILI9341_BLACK);
 	unsigned long start = micros();
-	tft.setCursor(0, 0);
-	tft.setTextColor(ILI9341_WHITE);  tft.setTextSize(1);
+	tft.setFont(&FreeSansBold24pt7b);
+	tft.setCursor(0, 36);
+	tft.setTextColor(ILI9341_WHITE); // tft.setTextSize(1);
 	tft.println("Hello World!");
-	tft.setTextColor(ILI9341_YELLOW); tft.setTextSize(2);
+	tft.setTextColor(ILI9341_YELLOW);// tft.setTextSize(2);
 	tft.println(1234.56);
-	tft.setTextColor(ILI9341_RED);    tft.setTextSize(3);
-	tft.println(0xDEADBEEF, HEX);
-	tft.println();
+	tft.setTextColor(ILI9341_RED);   // tft.setTextSize(3);
+	tft.println(0x121212, HEX);
 	tft.setTextColor(ILI9341_GREEN);
-	tft.setTextSize(5);
-	tft.println("Groop");
-	tft.setTextSize(2);
-	tft.println("I implore thee,");
-	tft.setTextSize(1);
-	tft.println("my foonting turlingdromes.");
-	tft.println("And hooptiously drangle me");
-	tft.println("with crinkly bindlewurdles,");
-	tft.println("Or I will rend thee");
-	tft.println("in the gobberwarts");
-	tft.println("with my blurglecruncheon,");
-	tft.println("see if I don't!");
+	//tft.setTextSize(5);
+	tft.println("Bisous!");
+	//tft.setTextSize(2);
 	return micros() - start;
 }
 
