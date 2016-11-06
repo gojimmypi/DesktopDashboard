85ee24e7f143f1ce55dd287de2455ead0cceb6fa
gojimmypi <gojimmypi@gmail.com>
2016-11-06 09:52:32 -0800

    Add project files.

---
+++ DesktopDashboard.ino
@@ -0,0 +1,15 @@
+// created in Visual Studio 2015 with Visual Micro add-in:  vMicro - New Arduino Project
+
+void setup()
+{
+
+  /* add setup code here */
+
+}
+
+void loop()
+{
+
+  /* add main program code here */
+
+}
