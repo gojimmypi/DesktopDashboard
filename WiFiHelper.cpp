37198d960af7946289990946f2340e18108c27c6
gojimmypi <gojimmypi@gmail.com>
2018-07-04 19:54:55 -0700

    WIFI_DEBUG and HEAP_DEBUG

--- WiFiHelper.cpp
+++ WiFiHelper.cpp
@@ -15,24 +15,23 @@
 //
 //*******************************************************************************************************************************************
 int wifiConnect(int maxAttempts) {
-#ifdef WIFI_DEBUG
-	Serial.println(DEBUG_SEPARATOR);
-	Serial.print("wifiConnect: Connecting to ");
-	Serial.println(WIFI_SSID);
-	Serial.println(DEBUG_SEPARATOR);
-#endif
+	WIFI_DEBUG_PRINTLN(DEBUG_SEPARATOR);
+	WIFI_DEBUG_PRINT("wifiConnect: Connecting to ");
+	WIFI_DEBUG_PRINTLN(WIFI_SSID);
+	WIFI_DEBUG_PRINTLN(DEBUG_SEPARATOR);
 
 	int countAttempt = 0;
+	HEAP_DEBUG_PRINTLN(DEFAULT_DEBUG_MESSAGE);
 	WiFi.mode(WIFI_STA);
+	HEAP_DEBUG_PRINTLN(DEFAULT_DEBUG_MESSAGE);
 	WiFi.begin(WIFI_SSID, WIFI_PWD);
-	
+	HEAP_DEBUG_PRINTLN(DEFAULT_DEBUG_MESSAGE);
+
 	String myMacAddress;
 	myMacAddress = WiFi.macAddress(); // this returns 6 hex bytes, delimited by colons
 	screenMessage("MAC Address", myMacAddress.substring(0, 9), myMacAddress.substring(9, 18)); // 01:34:67:90:12:45
 
-#ifdef WIFI_DEBUG
-	Serial.println("Starting Connection Loop...");
-#endif
+	WIFI_DEBUG_PRINTLN("Starting WiFi Connection Loop...");
 	while (WiFi.status() != WL_CONNECTED) {  // try to connect wifi for 6 sec then reset
 
 											 // this tft code is not actualy DOING anything yet
@@ -42,33 +41,26 @@ int wifiConnect(int maxAttempts) {
 		tft.setTextColor(ILI9341_RED);
 		tft.setCursor(15, 195);
 
-#ifdef WIFI_DEBUG
-		Serial.print(".");
-#endif
+		WIFI_DEBUG_PRINT(".");
 		delay(250);
 		countAttempt++;
 		if (countAttempt > maxAttempts) {
 			countAttempt = 0;
-#ifdef WIFI_DEBUG
-			Serial.println("WiFi Disconnect... ");
-#endif
+			WIFI_DEBUG_PRINTLN("WiFi Disconnect... ");
 			WiFi.disconnect();
 			delay(5000);
-#ifdef WIFI_DEBUG
-			Serial.println("WiFi Retrying. ");
-#endif
+			WIFI_DEBUG_PRINTLN("WiFi Retrying. ");
 			WiFi.mode(WIFI_STA);
 			WiFi.begin(WIFI_SSID, WIFI_PWD);
 		}
 	}
-#ifdef WIFI_DEBUG
-	Serial.println("Connected!");
-#endif
+	WIFI_DEBUG_PRINTLN("Connected!");
 	delay(5000);
 	myMacAddress.replace(":", "");
 	myMacAddress.replace("-", ""); // probably not used, but just in case they MAC address starts returning other well known delimiters such as dash
 	myMacAddress.replace(" ", ""); // or perhaps even a space
 
 	Serial.println("MAC Address=" + myMacAddress);
+	HEAP_DEBUG_PRINTLN(DEFAULT_DEBUG_MESSAGE);
 	return 0;
 }
