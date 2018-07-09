dfb7d6f6dd23dba8bdf0b222dc6de6fbdf21dde2
gojimmypi <gojimmypi@gmail.com>
2018-07-09 15:34:23 -0700

    conditional compile for ESP32 (Alternate named WiFi library)

--- WiFiHelper.cpp
+++ WiFiHelper.cpp
@@ -2,8 +2,27 @@
 
 #include "WiFiHelper.h"
 
-#include <ESP8266HTTPClient.h> // includes WiFiClient.h
+//#include <ESP8266HTTPClient.h> // includes WiFiClient.h
+//#include <ESP8266WiFi.h>
+
+#undef FOUND_BOARD
+#ifdef ARDUINO_ARCH_ESP8266
+#include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
+#define FOUND_BOARD ESP8266
+#endif
+
+#ifdef ARDUINO_ARCH_ESP32
+#include <HTTPClient.h>
+#include <WiFi.h>
+#define FOUND_BOARD ESP32
+#endif
+
+#ifndef FOUND_BOARD
+#pragma message(Reminder "Error Target hardware not defined !")
+#endif // ! FOUND_BOARD
+
+
 #include "tftHelper.h"
 
 
